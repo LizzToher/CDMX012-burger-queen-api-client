bd40f8f1a6f297d87fc8e033814081784f47425c
rocio <rholeon@gmail.com>
2022-06-06 20:41:29 -0500

    modificación en la funcionalidad del decremento de los elementos del pedido, aplicación de css a la vista del mesero

--- my-app/src/components/waiterView/OrdersView.jsx
+++ my-app/src/components/waiterView/OrdersView.jsx
@@ -1,26 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
+import OrderTable from './OrderTable';
 import styles from './WaiterView.module.css';
 
 const OrdersView = ({ orders, setOrders }) => {
-
-  const [counter, setCounter] = useState(1);
 
   const removeProductFromOrder = (productId) => {
     const orderIndex = orders.findIndex(order => order.id === productId);
     const removedProduct = ([...orders.slice(0, orderIndex), ...orders.slice(orderIndex + 1)]);
     setOrders(removedProduct);
-    setCounter(counter + 1);
-    if (removeProductFromOrder) {
-      setCounter(1);
-    }
   };
 
-  // const totalPriceOfCart = (orders) => {
   const totalAmountCount = (Object.values(orders).reduce((acum, { quantity, price }) => acum + quantity * price, 0));
-  console.log('precio total', totalAmountCount);
-  //   console.log('precio total', totalPriceOfCart);
-
-  // };
 
   const incrementClik = (productId) => {
     const updateOrders = orders.map((order) => {
@@ -36,70 +26,24 @@ const OrdersView = ({ orders, setOrders }) => {
 
   const decrementClik = (productId) => {
     const updateOrders = orders.map((order) => {
+      const orderQuantity = order.quantity;
+      if (orderQuantity === 1) {
+        return order;
+      }
+      if (productId === order.id) {
         const orderQuantity = order.quantity;
-        if(orderQuantity === 1){
-          return console.log({...order, quantity: parseInt(orderQuantity)});
-        } else {
-          if(productId === order.id){
-          return {...order, quantity: orderQuantity - 1};
-          }
-        }
-    
+        return { ...order, quantity: orderQuantity - 1 };
+      } else {
+        return order;
+      }
     });
-    setOrders(updateOrders);  
+    setOrders(updateOrders);
   };
 
   return (
     <div className={styles.container}>
-    <div className={styles.menuOrderContainer}>
-      <article className={`${styles.split} ${styles.right}`}>
-        <section className={styles.centered} >
-          <section className={styles.orderContainer}>
-            <h1>Órdenes</h1>
-            <section className={styles.tableContainer}>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Producto</th>
-                    <th>Precio</th>
-                    <th>Cantidad</th>
-                    <th>Acción</th>
-                  </tr>
-                </thead>
-                {orders &&
-                  orders.map((product) => {
-                    return (
-                      <tbody key={product.id} >
-                        <tr>
-                          <td>{product.product}</td>
-                          <td>${product.price}</td>
-                          <td className={styles.counterContainer}>
-                            <button clasName={styles.counterbtn} onClick={() => decrementClik(product.id)}>-</button>
-                            <p>{product.quantity}</p>
-                            <button clasName={styles.counterbtn} onClick={() => incrementClik(product.id)}>+</button>
-                          </td>
-                          <td>
-                            <button className={styles.deleteProduct} onClick={() => removeProductFromOrder(product.id)}>Eliminar</button>
-                          </td>
-                        </tr>
-                      </tbody>
-                    );
-                  })
-                }
-                <tfoot>
-                  <tr>
-                    <td>Total</td>
-                    <td>${totalAmountCount}</td>
-                    <td></td>
-                    <button className={styles.sendProduct}>Enviar pedido</button>
-                  </tr>
-                </tfoot>
-                </table>
-            </section>
-          </section>
-        </section>
-      </article>
-    </div>
+        <OrderTable orders={orders} incrementClik={incrementClik} decrementClik={decrementClik}
+          removeProductFromOrder={removeProductFromOrder} totalAmountCount={totalAmountCount} />
     </div>
   );
 };
