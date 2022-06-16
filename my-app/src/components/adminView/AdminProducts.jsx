cfa5aceade3e287bec8e1a303cec42ad3d73df73
rocio <rholeon@gmail.com>
2022-06-16 11:29:49 -0500

    funcionalidad para agregar productos en vista del admin

--- my-app/src/components/adminView/AdminProducts.jsx
+++ my-app/src/components/adminView/AdminProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logoSmall from '../../assets/logo-nav_small.png';
 import logout from '../../assets/logout.png';
 import edit from '../../assets/edit.png';
@@ -6,76 +6,80 @@ import delete1 from '../../assets/delete1.png';
 import styles from './AdminView.module.css';
 import AddProduct from './productsComponents/AddProduct';
 
-const AdminProducts = ({ products, setNavSection, handleLogOut }) => {
-  const [addProduct, setAddProduct] = useState('addProduct');
-  if(addProduct === 'addProduct') {
+const AdminProducts = ({ products, setNavSection, handleLogOut, navSection }) => {
+  // const [addProduct, setAddProduct] = useState('addProduct');
+  
+  if(navSection === 'products'){
+    return (
+      <div className={styles.container}>
+        <header className={styles.headerContainer}>
+          <img className={styles.logosmall} src={logoSmall} alt="small logo" />
+          <h2 className={styles.menuLetter} onClick={() => setNavSection('employees')}>Trabajadores</h2>
+          <h2 className={styles.menuLetter} onClick={() => setNavSection('products')}>Productos</h2>
+          <img
+            className={styles.logout}
+            src={logout}
+            alt="logout"
+            onClick={handleLogOut}
+          />
+        </header>
+        <section>
+          <table>
+            <thead>
+              <tr>
+                <th>Producto</th>
+                <th>Precio</th>
+                <th>Categoría</th>
+                <th>Acción</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products &&
+                products.map((product) => {
+                  return (
+                    <>
+                      <tr key={product.id}>
+                        <td >{product.product}</td>
+                        <td >{product.price}</td>
+                        <td >{product.category}</td>
+                        <td className={styles.buttonContainer}>
+                          <img
+                            className={styles.action}
+                            src={edit}
+                            alt="edit"
+                            // onClick={handleLogOut}
+                          />
+                          <br />
+                          <img
+                            className={styles.action}
+                            src={delete1}
+                            alt="delete"
+                          // onClick={handleLogOut}
+                          />
+                        </td>
+                      </tr>
+                    </>
+                  );
+                })}
+            </tbody>
+          </table>
+        </section>
+        <div>
+          <button className={styles.deleteProduct} onClick={() => setNavSection('addProduct')}>Agregar</button>
+        </div>
+      </div>
+    );
+  }
+ 
+  if(navSection === 'addProduct') {
     return (
       <>
-        <AddProduct setAddProduct={setAddProduct} products={products} setNavSection={setNavSection} handleLogOut={handleLogOut}/>
+        <AddProduct products={products} setNavSection={setNavSection} handleLogOut={handleLogOut} />
       </>
     );
   }
 
-  return (
-    <div className={styles.container}>
-      <header className={styles.headerContainer}>
-        <img className={styles.logosmall} src={logoSmall} alt="small logo" />
-        <h2 className={styles.menuLetter} onClick={() => setNavSection('employees')}>Trabajadores</h2>
-        <h2 className={styles.menuLetter} onClick={() => setNavSection('products')}>Productos</h2>
-        <img
-          className={styles.logout}
-          src={logout}
-          alt="logout"
-          onClick={handleLogOut}
-        />
-      </header>
-      <section>
-        <table>
-          <thead>
-            <tr>
-              <th>Producto</th>
-              <th>Precio</th>
-              <th>Categoría</th>
-              <th>Acción</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products &&
-              products.map((product) => {
-                return (
-                  <>
-                    <tr key={product.id}>
-                      <td >{product.product}</td>
-                      <td >{product.price}</td>
-                      <td >{product.category}</td>
-                      <td className={styles.buttonContainer}>
-                        <img
-                          className={styles.action}
-                          src={edit}
-                          alt="edit"
-                        // onClick={handleLogOut}
-                        />
-                        <br />
-                        <img
-                          className={styles.action}
-                          src={delete1}
-                          alt="delete"
-                        // onClick={handleLogOut}
-                        />
-                      </td>
-                    </tr>
-                  </>
-                );
-              })}
-          </tbody>
-        </table>
-      </section>
-      <div>
-        <button className={styles.deleteProduct} onClick={() => setAddProduct('addProduct')}>Agregar</button>
-      </div>
-    </div>
-  );
-
 };
+
 
 export default AdminProducts;
--- my-app/src/components/adminView/productsComponents/AddProduct.jsx
+++ my-app/src/components/adminView/productsComponents/AddProduct.jsx
@@ -2,29 +2,64 @@ import React from 'react';
 import logoSmall from '../../../assets/logo-nav_small.png';
 import logout from '../../../assets/logout.png';
 import styles from '../AdminView.module.css';
+import { useForm } from 'react-hook-form';
+import addProduct from '../../../hooks/SaveProducts';
 
 
-const AddProduct = ({products, setNavSection, handleLogOut, setAddProduct}) => {
-    console.log(products);
+const AddProduct = ({ products, setNavSection, handleLogOut }) => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+console.log(products);
+  const saveProductToData = (newProduct) => {
+console.log('esto lo paso por parametro a saveproduct' , newProduct);
+      addProduct(newProduct);
+   
+    };
+    // console.log(saveProductToData);
+  
+
   return (
     <div className={styles.container}>
-    <header className={styles.headerContainer}>
-      <img className={styles.logosmall} src={logoSmall} alt="small logo" />
-      <h2 className={styles.menuLetter} onClick={() => setNavSection('employees')}>Trabajadores</h2>
-      <h2 className={styles.menuLetter} onClick={() => setAddProduct('regresar')}>Productos</h2>
-      <img
-        className={styles.logout}
-        src={logout}
-        alt="logout"
-        onClick={handleLogOut}
-      />
-    </header>
-    <div>
+      <header className={styles.headerContainer}>
+        <img className={styles.logosmall} src={logoSmall} alt="small logo" />
+        <h2 className={styles.menuLetter} onClick={() => setNavSection('employees')}>Trabajadores</h2>
+        <h2 className={styles.menuLetter} onClick={() => setNavSection('products')}>Productos</h2>
+        <img
+          className={styles.logout}
+          src={logout}
+          alt="logout"
+          onClick={handleLogOut}
+        />
+      </header>
+      <button onClick={() => setNavSection('products')}>regresar</button>
+      <fieldset>
+        <div className='titleHeader'>
+          <h1>Registrar Producto</h1>
+        </div>
+        <form className='formContainer' onSubmit={handleSubmit(saveProductToData)}>
+          <div>
+            <input {...register('Producto', { required: true, maxLength: 20 })} />
+            {errors.Producto?.type === 'required' && 'Campo Producto es requerido'}
+          </div>
+          <div>
+            <input type='number' {...register('Precio', { required: true, min: 2, max: 6 })} />
+            {errors.Precio?.type === 'required' && 'Campo Precio es requerido'}
+          </div>
+          <select className={styles.selectValue} {...register('Menú', {required: true}) } >
+            <option value={'desayuno'}>Desayuno</option>
+            <option value={'almuerzo'}>Almuerzo</option>
+            {errors.Menú?.type === 'required' && 'Campo Menú es requerido'}
+          </select>
 
+          <button
+            className="loginButton1" type='submit'  >
+            Agregar Product
+          </button>
 
-    </div>
+        </form>
+      </fieldset>
     </div>
   );
 };
+
 
 export default AddProduct;
